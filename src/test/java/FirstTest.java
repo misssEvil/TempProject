a8f13c3ba5981c9d623f98048adf6485e6a98073
Adelina <misssluv@yandex.ru>
2022-06-18 18:01:57 +0300

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class FirstTest extends BaseTest {
+
+    @Test
+    public void example(){
+        getDriver().get("http://www.99-bottles-of-beer.net/");
+
+        getDriver().findElement(By.xpath("//*[@id='submenu']/li[2]/a")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//*[@id='main']/h2")).getText(),
+                "Lyrics of the song 99 Bottles of Beer");
+    }
+}
---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,24 @@
+package runner;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        driver = BaseUtils.createDriver();
+    }
+
+    @AfterMethod
+    protected void afterMethod() {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+}
