e040611a585c680ba17d4988444d5cec8bd15268
Adelina <misssluv@yandex.ru>
2022-06-22 23:47:32 +0300

    return string format back

--- src/test/java/runner/JenkinsUtils.java
+++ src/test/java/runner/JenkinsUtils.java
@@ -13,7 +13,7 @@ public final class JenkinsUtils {
    private static final String PASSWORD = PREFIX + "admin.password";
 
     static void getBaseUrl(WebDriver driver){
-        driver.get("http://localhost:8080" );
+        driver.get(String.format("http://localhost:%s", getProperties().getProperty(PORT)));
     }
 
     static void login(WebDriver driver) {
