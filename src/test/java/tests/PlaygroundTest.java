1834ee20168b6e96a207b9e7a4bde55bb751e262
Adelina <misssluv@yandex.ru>
2022-06-22 22:39:02 +0300

    try jenkins test with fixed url

---
+++ src/test/java/tests/PlaygroundTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class PlaygroundTest extends BaseTest {
+
+    @Test
+    public void randomTest() {
+
+        WebElement searchBox = getDriver().findElement(By.id("search-box"));
+        searchBox.sendKeys("0000");
+        searchBox.sendKeys(Keys.ENTER);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Search for '0000'");
+
+    }
+}
